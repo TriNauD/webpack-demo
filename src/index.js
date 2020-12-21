9f4f7a193b6ea1aba54c1b2e9dc6d7c49f22d787
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-21 15:42:09 +0800

    Use react to write the page.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+// import './style.css';
+// import Logo from './logo.png';
+//
+// function component() {
+//     const element = document.createElement('div');
+//     //lodash now imported by this script
+//     element.innerHTML = _.join(['Hello','webpack'],' ');
+//     element.classList.add('hello');
+//
+//     //add pic
+//     const myLogo = new Image();
+//     myLogo.src = Logo;
+//
+//     element.appendChild(myLogo);
+//
+//     return element;
+// }
+// document.body.appendChild(component());
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.css';
-import Logo from './logo.png';
 
-function component() {
-    const element = document.createElement('div');
-    //lodash now imported by this script
-    element.innerHTML = _.join(['Hello','webpack'],' ');
-    element.classList.add('hello');
-
-    //add pic
-    const myLogo = new Image();
-    myLogo.src = Logo;
-
-    element.appendChild(myLogo);
-
-    return element;
+function formatName(user) {
+    return (
+        user.props.firstName + ' ' + user.props.lastName
+    );
 }
-document.body.appendChild(component());
+
+function greetingUser(user) {
+    if (user.props.firstName != null && user.props.lastName != null) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    } else {
+        return <h1>Hello, stranger!</h1>;
+    }
+}
+
+const user = {
+    id: 'student1',
+    props: {
+        firstName: 'Imai',
+        lastName: 'Lisa',
+    }
+}
+
+const element = (
+    greetingUser(user)
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById("root")
+);

1bf6655707d48fdd86e05a0a38499ee33fe74b64
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-22 14:47:56 +0800

    Start working on canvas. Finish basic layout and get the position of mouse up&down.

--- src/index.js
+++ src/index.js
@@ -1,53 +1,83 @@
-// import _ from 'lodash';
-// import './style.css';
-// import Logo from './logo.png';
-//
-// function component() {
-//     const element = document.createElement('div');
-//     //lodash now imported by this script
-//     element.innerHTML = _.join(['Hello','webpack'],' ');
-//     element.classList.add('hello');
-//
-//     //add pic
-//     const myLogo = new Image();
-//     myLogo.src = Logo;
-//
-//     element.appendChild(myLogo);
-//
-//     return element;
-// }
-// document.body.appendChild(component());
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-function formatName(user) {
-    return (
-        user.props.firstName + ' ' + user.props.lastName
-    );
+class MyCanvas extends React.Component {
+    constructor(props) {
+        super(props);
+        this.canvas = React.createRef();
+        // this.handleClick = this.handleClick.bind(this);
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
+    }
+
+    // handleClick() {
+    //     const canvas = this.canvas.current;
+    //     const ctx = canvas.getContext('2d');
+    //     ctx.strokeStyle = "#7c8b8c";
+    //     ctx.line(90, 130, 320, 210);
+    // }
+
+    onMouseDown = (e) => {
+        const canvas = this.canvas.current;
+        const ctx = canvas.getContext('2d');
+        console.log('mouse down');
+        console.log('x:' + (e.clientX - canvas.offsetLeft) + 'y:' + (e.clientY - canvas.offsetTop));
+    }
+
+    onMouseUp = (e) => {
+        const canvas = this.canvas.current;
+        const ctx = canvas.getContext('2d');
+        console.log('mouse up');
+        console.log('x:' + (e.clientX - canvas.offsetLeft) + 'y:' + (e.clientY - canvas.offsetTop));
+    }
+
+    line(x, y, x1, y1) {
+        this.save();
+        this.beginPath();
+        this.moveTo(x, y);
+        this.lineTo(x1, y1);
+        this.stroke();
+        this.restore();
+    }
+
+    componentDidMount() {
+        const canvas = this.canvas.current;
+        const ctx = canvas.getContext('2d');
+        (function () {
+            Object.getPrototypeOf(ctx).line = function (x, y, x1, y1) {
+                this.save();
+                this.beginPath();
+                this.moveTo(x, y);
+                this.lineTo(x1, y1);
+                this.stroke();
+                this.restore();
+            }
+        })();
+
+    }
+
+
+    render() {
+        return (
+            <canvas
+                height="600px" width="800px" ref={this.canvas}
+                onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp}>
+            </canvas>
+        );
+    }
+
 }
 
-function greetingUser(user) {
-    if (user.props.firstName != null && user.props.lastName != null) {
-        return <h1>Hello, {formatName(user)}!</h1>;
-    } else {
-        return <h1>Hello, stranger!</h1>;
+class App extends React.Component {
+    render() {
+        return (
+            <MyCanvas/>
+        );
     }
 }
-
-const user = {
-    id: 'student1',
-    props: {
-        firstName: 'Imai',
-        lastName: 'Lisa',
-    }
-}
-
-const element = (
-    greetingUser(user)
-);
 
 ReactDOM.render(
-    element,
-    document.getElementById("root")
+    <App/>,
+    document.getElementById('root')
 );
